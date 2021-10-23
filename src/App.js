bc72e6197d5c8c216207b3b2b140e8fd8d6104ad
Quentin Dincuff <redkilleur29@gmail.com>
2021-10-23 07:35:35 +0200

    mise en production

--- src/App.js
+++ src/App.js
@@ -1,50 +1,77 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+import React, { useState } from 'react';
+const api = {
+  key: "ca37cdafd192434bdfc3c40733cee944",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+          .then(res => res.json())
+          .then(result => {
+            setWeather(result);
+            setQuery('');
+            console.log(result);
+          });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
+    let days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  const heureBuilder = (h) => {
+    let hour = h.getHours();
+    let min = h.getMinutes();
+
+    return `${hour}h ${min}min`
+  }
+
+  return (
+      <div className={(typeof weather.main != "undefined")
+          ? ((weather.main.temp > 16) ? 'app warm' : 'app')
+          || ((weather.main.temp < 5) ? 'app cold' : 'app')
+          : 'app'}>
+        <main>
+          <div className="search-box">
+            <input
+                type="text"
+                className="search-bar"
+                placeholder="Ville..."
+                onChange={e => setQuery(e.target.value)}
+                value={query}
+                onKeyPress={search}
+            />
+          </div>
+          {(typeof weather.main != "undefined") ? (
+              <div>
+                <div className="location-box">
+                  <div className="location">{weather.name}, {weather.sys.country}</div>
+                  <div className="date">{dateBuilder(new Date())}</div>
+                  <div className="heure">{heureBuilder(new Date())}</div>
+                </div>
+                <div className="weather-box">
+                  <div className="temp">
+                    {Math.round(weather.main.temp)}°c
+                  </div>
+                  <div className="weather">{weather.weather[0].description}</div>
+                </div>
+              </div>
+          ) : ('')}
+        </main>
       </div>
-    )
-  }
+  );
 }
 
-export default App
+export default App;
